2098b65133629e8b7d799635083a58fadc9e2f2c
Muhammad Hassan Ali <mhali@dawaai.pk>
2021-09-12 17:28:22 +0500

    Added databinding to cart

--- app/src/main/java/com/hassan/sadaparceltest/view/cart/CartFragment.kt
+++ app/src/main/java/com/hassan/sadaparceltest/view/cart/CartFragment.kt
@@ -5,28 +5,32 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.hassan.sadaparceltest.R
+import com.hassan.sadaparceltest.databinding.FragmentCartBinding
 import com.hassan.sadaparceltest.viewmodel.cart.CartViewModel
 
 class CartFragment : Fragment() {
 
-    private lateinit var cartViewModel: CartViewModel
+    private val cartViewModel by viewModels<CartViewModel>()
+    private lateinit var binding: FragmentCartBinding
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        cartViewModel =
-                ViewModelProvider(this).get(CartViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_cart, container, false)
-        val textView: TextView = root.findViewById(R.id.text_dashboard)
-        cartViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+
+        binding = DataBindingUtil.inflate(layoutInflater,R.layout.fragment_cart,container,false)
+        binding.cartViewModel = cartViewModel
+        binding.lifecycleOwner = this
+//        cartViewModel.text.observe(viewLifecycleOwner, Observer {
+//            textView.text = it
+//        })
+        return binding.root
     }
 }
