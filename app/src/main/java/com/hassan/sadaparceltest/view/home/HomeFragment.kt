810ed7c2032a7ce77713d488ec1203d10b6a3d87
Muhammad Hassan Ali <mhali@dawaai.pk>
2021-09-12 17:32:35 +0500

    Added databinding to home

--- app/src/main/java/com/hassan/sadaparceltest/view/home/HomeFragment.kt
+++ app/src/main/java/com/hassan/sadaparceltest/view/home/HomeFragment.kt
@@ -5,28 +5,30 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.hassan.sadaparceltest.R
+import com.hassan.sadaparceltest.databinding.FragmentHomeBinding
 import com.hassan.sadaparceltest.viewmodel.home.HomeViewModel
 
 class HomeFragment : Fragment() {
 
-    private lateinit var homeViewModel: HomeViewModel
+    private val homeViewModel by viewModels<HomeViewModel>()
+    private lateinit var binding: FragmentHomeBinding
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        homeViewModel =
-                ViewModelProvider(this).get(HomeViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+
+         binding = DataBindingUtil.inflate(inflater,R.layout.fragment_home, container, false)
+        binding.homeViewModel = homeViewModel
+        binding.lifecycleOwner = this
+
+        return binding.root
     }
 }
