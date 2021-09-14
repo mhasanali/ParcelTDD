d734664a65703223afed84d6e95c02381afbc35c
Muhammad Hassan Ali <mhali@dawaai.pk>
2021-09-15 00:11:10 +0500

    Test case passed for cart screen title

--- app/src/main/java/com/hassan/sadaparceltest/viewmodel/cart/CartViewModel.kt
+++ app/src/main/java/com/hassan/sadaparceltest/viewmodel/cart/CartViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class CartViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is cart Fragment"
+        value = "Cart"
     }
     val text: LiveData<String> = _text
 }
